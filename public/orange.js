62351b6261a4794b3e4736e9010ebaf65fdf5a92
Sai Bhargavi <95035534+SaiBharga@users.noreply.github.com>
2021-12-21 22:41:32 +0530

    orange.js code optimized

--- public/orange.js
+++ public/orange.js
@@ -1,31 +1,20 @@
-
 const orange_increment = document.getElementById('orange_increment');
+
 const orange_decrement = document.getElementById('orange_decrement');
 
 
 const orange_count_display = document.getElementById('orange_count_display');
 
-const orange = localStorage.getItem('orange') | 0   ;
+orange_count_display.textContent = localStorage.getItem('orange') ?? "0";
 
-orange_count_display.textContent = orange ? orange : '0';
-
-function increment_orange() {
+function handleClick(event){
   const old_value = localStorage.getItem("orange") | 0;
-  const new_value = old_value + 1;
+  const new_value = event.currentTarget.id === "orange_increment" ? (old_value + 1) : (Math.max(0, old_value - 1));
   localStorage.setItem("orange", new_value);
-  orange_count_display.textContent = new_value? new_value: "0";
+  blue_count_display.textContent = new_value? new_value: "0";
 }
 
-function decrement_orange() {
-    const old_value = localStorage.getItem("orange") | 0;
-    const new_value = Math.max(0, old_value - 1);
-    localStorage.setItem("orange", new_value);
-    orange_count_display.textContent = new_value? new_value: "0";
-  }
 
+orange_increment.addEventListener('click', handleClick);
 
-
-orange_increment.addEventListener('click', increment_orange);
-
-orange_decrement.addEventListener('click', decrement_orange);
-
+orange_decrement.addEventListener('click', handleClick);

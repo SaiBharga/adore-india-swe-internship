83728150a1cd80ba161478134be444d570d8c3ec
Sai Bhargavi <95035534+SaiBharga@users.noreply.github.com>
2021-12-21 22:42:55 +0530

    pink.js code optimized

--- public/pink.js
+++ public/pink.js
@@ -1,31 +1,21 @@
 
 const pink_increment = document.getElementById('pink_increment');
+
 const pink_decrement = document.getElementById('pink_decrement');
 
 
 const pink_count_display = document.getElementById('pink_count_display');
 
-const pink = localStorage.getItem('pink') | 0   ;
+pink_count_display.textContent = localStorage.getItem('pink') ?? "0";
 
-pink_count_display.textContent = pink ? pink : '0';
-
-function increment_pink() {
+function handleClick(event){
   const old_value = localStorage.getItem("pink") | 0;
-  const new_value = old_value + 1;
+  const new_value = event.currentTarget.id === "pink_increment" ? (old_value + 1) : (Math.max(0, old_value - 1));
   localStorage.setItem("pink", new_value);
-  pink_count_display.textContent = new_value? new_value: "0";
+  blue_count_display.textContent = new_value? new_value: "0";
 }
 
-function decrement_pink() {
-    const old_value = localStorage.getItem("pink") | 0;
-    const new_value = Math.max(0, old_value - 1);
-    localStorage.setItem("pink", new_value);
-    pink_count_display.textContent = new_value? new_value: "0";
-  }
 
+pink_increment.addEventListener('click', handleClick);
 
-
-pink_increment.addEventListener('click', increment_pink);
-
-pink_decrement.addEventListener('click', decrement_pink);
-
+pink_decrement.addEventListener('click', handleClick);

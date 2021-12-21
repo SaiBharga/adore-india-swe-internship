bc2ce75ed16df6234a3fd49c7fcb1fb9f1365721
Sai Bhargavi <95035534+SaiBharga@users.noreply.github.com>
2021-12-21 21:08:28 +0530

    green.js code optimized

--- public/green.js
+++ public/green.js
@@ -1,31 +1,19 @@
 
 const green_increment = document.getElementById('green_increment');
+
 const green_decrement = document.getElementById('green_decrement');
 
 
 const green_count_display = document.getElementById('green_count_display');
 
-const green = localStorage.getItem('green') | 0   ;
+green_count_display.textContent = localStorage.getItem('green') ?? "0";
 
-green_count_display.textContent = green ? green : '0';
-
-function increment_green() {
+function handleClick(event){
   const old_value = localStorage.getItem("green") | 0;
-  const new_value = old_value + 1;
+  const new_value = event.currentTarget.id === "green_increment" ? (old_value + 1) : (Math.max(0, old_value - 1));
   localStorage.setItem("green", new_value);
-  green_count_display.textContent = new_value? new_value: "0";
+  green_count_display.textContent = new_value ? new_value : "0";
 }
 
-function decrement_green() {
-    const old_value = localStorage.getItem("green") | 0;
-    const new_value = Math.max(0, old_value - 1);
-    localStorage.setItem("green", new_value);
-    green_count_display.textContent = new_value? new_value: "0";
-  }
-
-
-
-green_increment.addEventListener('click', increment_green);
-
-green_decrement.addEventListener('click', decrement_green);
-
+green_increment.addEventListener('click', handleClick);
+green_decrement.addEventListener('click', handleClick);
